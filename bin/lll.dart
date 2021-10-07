2e1f45e2bea82fd5ca905c58ffbcb3be4d0b3e00
BeggiNerd00 <bjartmar.hallgrimsson@gmail.com>
2021-10-07 13:22:35 +0000

    Answer 1

---
+++ bin/lll.dart
@@ -0,0 +1,5 @@
+import 'package:lll/lll.dart' as lll;
+
+void main(List<String> arguments) {
+  print('Hello world: ${lll.calculate()}!');
+}
---
+++ test/lll_test.dart
@@ -0,0 +1,8 @@
+import 'package:lll/lll.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
